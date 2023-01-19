3bcca4599f466f7272a3800bb97d287e35fb2346
Dmitry Aleksandrov <dmitry.aleksandrov@oracle.com>
2023-01-19 12:03:56 +0200

    Initial commit

---
+++ src/main/java/io/helidon/quickstart/Message.java
@@ -0,0 +1,32 @@
+package io.helidon.quickstart;
+
+
+public class Message {
+
+    private String message;
+
+    private String greeting;
+
+    public Message() {
+    }
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setGreeting(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return this.greeting;
+    }
+}
---
+++ src/test/java/io/helidon/quickstart/MainTest.java
@@ -0,0 +1,71 @@
+
+package io.helidon.quickstart;
+
+import org.eclipse.microprofile.metrics.Counter;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.core.MediaType;
+
+import io.helidon.microprofile.tests.junit5.HelidonTest;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@HelidonTest
+class MainTest {
+
+    @Inject
+    private MetricRegistry registry;
+
+    @Inject
+    private WebTarget target;
+
+    @Test
+    void testMetrics() {
+        Response response = target
+                .path("metrics")
+                .request()
+                .get();
+        assertThat(response.getStatus(), is(200));
+    }
+
+    @Test
+    void testHealth() {
+        Response response = target
+                .path("health")
+                .request()
+                .get();
+        assertThat(response.getStatus(), is(200));
+    }
+                
+    @Test
+    void testGreetings() {
+        Message jsonMessage = target
+                .path("greet/Joe")
+                .request()
+                .get(Message.class);
+        assertThat(jsonMessage.getMessage(), is("Hello Joe!"));
+
+        try (Response r = target
+                .path("greet/greeting")
+                .request()
+                .put(Entity.entity("{\"greeting\" : \"Hola\"}", MediaType.APPLICATION_JSON))) {
+            assertThat(r.getStatus(), is(204));
+        }
+
+        jsonMessage = target
+                .path("greet/Jose")
+                .request()
+                .get(Message.class);
+        assertThat(jsonMessage.getMessage(), is("Hola Jose!"));
+    }
+                
+}
